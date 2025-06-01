57168f0c96b3e36e4156a6e569b1375eff1343ee
Luis Gustavo <darkbj13.lg@gmail.com>
2025-05-31 21:23:15 -0300

    Alteração da propriedade "a" para "button" / Mudança no título do site.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,24 +4,24 @@ export default function Navbar() {
     <nav className="h-screen flex items-center justify-center">
       <ul className="flex flex-col gap-40 border-1 w-full h-full justify-center">
         <li className="rotate-90 relative">
-          <a className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100" href="#">
+          <button className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100 cursor-pointer" href="#">
             Sobre mim
-          </a>
+          </button>
         </li>
         <li className="rotate-90 relative">
-          <a className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100" href="#">
+          <button className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100 cursor-pointer" href="#">
             Habilidades
-          </a>
+          </button>
         </li>
         <li className="rotate-90 relative">
-          <a className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100" href="#">
+          <button className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100 cursor-pointer" href="#">
             Portifólio
-          </a>
+          </button>
         </li>
         <li className="rotate-90 relative">
-          <a className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100" href="#">
+          <button className="text-white transform font-medium text-[1.2rem] uppercase tracking-wider p-5   after:content-[''] after:absolute after:left-0 after:bottom-[-6px] after:w-full after:h-[3px] after:bg-white after:rounded-[5px] after:scale-x-0 after:origin-right after:transition-transform after:duration-500 hover:after:origin-left hover:after:scale-x-100 cursor-pointer" href="#">
             Contato
-          </a>
+          </button>
         </li>
       </ul>
     </nav>
