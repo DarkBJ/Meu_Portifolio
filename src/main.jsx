1b470d9ae98203f106f71d8bb378218f1e72b805
Luis Gustavo <darkbj13.lg@gmail.com>
2025-06-01 01:49:47 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import Navbar from './components/navbar';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="w-full h-[100vh] bg-gradient-to-tr from-black to-gray-900 lg:grid grid-cols-[2fr_8fr_1fr] dm:grid-rows-3  overflow-hidden">
-      <Leftbar />
+
+        <Leftbar />
 
       <main className='w-full h-full flex items-center justify-center'>
         <h1 className="text-3xl font-bold text-white">Conteúdo principal</h1>
